299446f0fe9c6b28926bdeca2262deb4d6ff32cf
Saeed-Moradi <46932480+Saeed-Moradi@users.noreply.github.com>
2021-10-15 10:58:03 +0330

    Delete KNN.py

--- KNN.py
+++
@@ -1,7 +0,0 @@
-import urllib
-import numpy as np
-import matplotlib.pyplot as pd
-from sklearn.preprocessing import StandardScaler
-
-emails1 = "Spam-Filtering-For-Persian-master/emails/spamtraining"
-emails1 = "Spam-Filtering-For-Persian-master/emails/spamtesting"
