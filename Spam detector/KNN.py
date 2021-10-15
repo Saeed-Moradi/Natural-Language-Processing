317b46cf93fc9a6ff8e0165456b5344403ce6ae9
Saeed-Moradi <46932480+Saeed-Moradi@users.noreply.github.com>
2021-10-15 10:59:45 +0330

    Spam detection using KNN

---
+++ Spam detector/KNN.py
@@ -0,0 +1,7 @@
+import urllib
+import numpy as np
+import matplotlib.pyplot as pd
+from sklearn.preprocessing import StandardScaler
+
+emails1 = "Spam-Filtering-For-Persian-master/emails/spamtraining"
+emails1 = "Spam-Filtering-For-Persian-master/emails/spamtesting"
